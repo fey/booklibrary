436454cb4235c2f6a799496efd923ed0ea764ebd
fey <feycot@gmail.com>
2017-02-09 22:04:08 +0500

    v1

--- index.php
+++ index.php
@@ -48,4 +48,25 @@ foreach ($result as $row) { // мы получили массив массиво
               'nameLast' => $row['nameLast'],
           );
 }
+
+try {
+  $sql = 'SELECT author.nameFirst, author.nameLast, count(books.id) as sumBooks
+            FROM author LEFT JOIN books
+            ON authorid = author.id
+            GROUP BY author.id
+            ORDER BY sumBooks DESC';
+$result = $pdo->query($sql);
+} catch (Exception $e) {
+  echo 'Ошибка при получении счетчика книг' .$e->getMessage();
+}
+foreach ($result as $row) {
+  $bookCounts[] = array(
+    'nameFirst'=> $row['nameFirst'],
+    'nameLast' => $row['nameLast'],
+    'sumbooks' => $row['sumBooks']
+  );
+}
+
+
+
 include 'books.html.php';
